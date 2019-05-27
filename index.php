5602b2fb61d991106907b75a6ed014da0253f1fc
Adeel Safdar <adeelsafdar007@gmail.com>
2019-05-27 14:40:51 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use \App\ActivityLog as ActivityLog;
+use \App\User as User;
+use \App\OepProfile as OepProfile;
+
+use Carbon\Carbon;
+use \Entrust as Entrust;
+
+class ActivityController extends Controller
+{
+    protected $activity_log;
+	
+	public function __construct()
+	{	
+	   $this->middleware('auth');
+	}
+	
+	public function oep_licence_activity_logs()
+	{
+		
+	}
+}
